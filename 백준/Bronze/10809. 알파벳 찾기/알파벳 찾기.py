1b322e636c62980fe8f761b5c97e2221bf36ee8a
zzq3902 <48148683+zzq3902@users.noreply.github.com>
2024-03-19 20:22:41 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,13 +1,8 @@
-# 1. [-1] * 26 리스트를 만든다
-# 2. 문자열을 입력받고, 문자열을 돌며 리스트의 (해당 문자의 아스키코드 - 97) 인덱스에 해당 문자의 위치를 입력한다.
-# 3. 다만, 해당 위치의 값이 -1이 아닐 경우 넘어간다.
+S = input()
+abc ='abcdefghijklmnopqrstuvwxyz'
 
-ans = [-1] * 26
-inp = input()
-
-for i in range(len(inp)):
-    num = ord(inp[i]) - 97
-    if ans[num] == -1:
-        ans[num] = i
-
-print(*ans)
+for i in abc:
+    if i in S:
+        print(S.index(i), end= ' ')
+    else:
+        print( -1, end =' ')
