07693d004bdd6f506c321d5216a2dd13a1638b30
zzq3902 <48148683+zzq3902@users.noreply.github.com>
2024-03-23 11:02:20 +0900

    [Bronze II] Title: 공 바꾸기, Time: 156 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,12 @@
+# list의 index는 바구니의 번호, 원소의 값은 공의 번호로 생각해 주어진 순서대로 공을 바꾼다.
+# 바구니와 공의 번호를 맞추기 위해 balls의 크기를 N+1개로 설정한다.
+
+N, M = map(int, input().split())
+balls = list(range(N+1))
+
+for _ in range(M):
+    i, j = map(int, input().split())
+    balls[i], balls[j] = balls[j], balls[i]
+
+for i in range(1, N+1):
+    print(balls[i], end=" ")
