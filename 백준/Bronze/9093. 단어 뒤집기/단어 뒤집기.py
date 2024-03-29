82541ed7c37a4990b061b29e62bb4a726a8669de
zzq3902 <48148683+zzq3902@users.noreply.github.com>
2024-03-29 20:16:02 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 572 ms, Memory: 110640 KB -BaekjoonHub

---
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
@@ -0,0 +1,12 @@
+# split()으로 단어를 나누어 리스트에 저장한 후, 각 문자열을 뒤집어 출력한다.
+
+T = int(input())
+
+for _ in range(T):
+    words = list(input().split())
+    
+    for i in range(len(words)):
+        words[i] = words[i][-1:-(len(words[i])+1):-1]
+        
+    print(" ".join(words))
+   
