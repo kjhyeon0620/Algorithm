8e0c38e49ecb5e95d8f72f5c9b7a9ee0ce0f9fe5
zzq3902 <48148683+zzq3902@users.noreply.github.com>
2024-04-06 20:35:42 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 524 ms, Memory: 214672 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -0,0 +1,23 @@
+# dictionary를 이용하여 해당 숫자의 개수를 출력한다.
+
+import sys
+
+
+input = sys.stdin.readline
+N = int(input())
+cards = list(map(int, input().split()))
+cnt = dict()
+
+for card in cards:
+    if cnt.get(card) is None:
+        cnt[card] = 1
+    else:
+        cnt[card] += 1
+
+M = int(input())
+
+for num in map(int, input().split()):
+    if cnt.get(num) is None:
+        print(0, end=" ")
+    else:
+        print(cnt[num], end=" ")
