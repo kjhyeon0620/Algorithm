11c0a7de548fb04f516b219e3e20d3cf7957fd50
zzq3902 <48148683+zzq3902@users.noreply.github.com>
2024-04-29 17:11:45 +0900

    [Silver I] Title: 단지번호붙이기, Time: 120 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -3,35 +3,30 @@
 N = int(input())
 board = []
 visited = [[False for _ in range(N)] for _ in range(N)]
-
 for _ in range(N):
     board.append(input())
-    
 cnt = 0
 ans = []
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-
 for i in range(N):
     for j in range(N):
         if not visited[i][j] and board[i][j] == "1":
             cnt += 1
-            tmp = 0
+            tmp = 1
+            visited[i][j] = True
             stack = [[i, j]]
-            
             while stack:
                 node = stack.pop()
                 n1, n2 = node[0], node[1]
-                if visited[n1][n2]:
-                    continue
-                tmp += 1
-                visited[n1][n2] = True
+
                 for d in range(4):
                     nx = n1 + dx[d]
                     ny = n2 + dy[d]
                     if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] and board[nx][ny] == "1":
+                        tmp += 1
+                        visited[nx][ny] = True
                         stack.append([nx, ny])
-                        
             ans.append(tmp)
 ans.sort()
 print(cnt)
