de83e50fb3dd0fcea8613b98d8ee167ba1937065
zzq3902 <48148683+zzq3902@users.noreply.github.com>
2024-06-30 00:12:28 +0900

    [Silver I] Title: 오르막 수, Time: 92 ms, Memory: 108416 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.py
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -1,20 +1,16 @@
-# 자릿수 별로 계산한다.
+# (n+9) C n
+
+def factorial(num):
+    if num == 1:
+        return 1
+    if memo[num] != -1:
+        return memo[num]
+    else:
+        memo[num] = factorial(num-1) * num
+        return memo[num]
+
 
 N = int(input())
-memo = [[-1] * 10 for _ in range(N+1)] # memo[n][k] => n번째 자리수가 k일 때 오르막 수의 개수
+memo = [-1] * (N+10)
 
-for a in range(10):     # 한 자리 수는 따로 계산
-    memo[1][a] = 10 - a
-
-for i in range(2, N+1):
-    for j in range(10):
-        if j == 0:
-            memo[i][j] = 0
-            for k in range(10):
-                memo[i][j] += memo[i-1][k]
-            memo[i][j] %= 10007
-        else:
-            memo[i][j] = memo[i][j-1] - memo[i-1][j-1]
-            memo[i][j] %= 10007
-
-print(memo[N][0])
+print((factorial(N+9) // factorial(N) // factorial(9)) % 10007)
