f67dec5accbfa7aebd7ca559204f12d462122d8c
zzq3902 <48148683+zzq3902@users.noreply.github.com>
2024-07-03 18:18:04 +0900

    [Silver I] Title: 그림, Time: 200 ms, Memory: 125492 KB -BaekjoonHub

---
+++ 백준/Silver/1926. 그림/그림.py
@@ -0,0 +1,41 @@
+# dfs를 이용하여 그림의 개수와 넓이를 구한다.
+
+import sys
+
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+board = []
+
+for _ in range(n):
+    board.append(list(map(int, input().split())))
+
+cnt = 0
+areas = []
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+for i in range(n):
+    for j in range(m):
+        if board[i][j] == 1:
+            board[i][j] = 0
+            cnt += 1
+            queue = [[i, j]]
+            tmp = 0
+            while queue:
+                node = queue.pop()
+                y, x = node[0], node[1]
+                tmp += 1
+                for a in range(4):
+                    ny = y + dy[a]
+                    nx = x + dx[a]
+                    if 0 <= ny < n and 0 <= nx < m and board[ny][nx] == 1:
+                        board[ny][nx] = 0
+                        queue.append([ny, nx])
+            areas.append(tmp)
+
+print(cnt)
+if len(areas) == 0:
+    print(0)
+else:
+    print(max(areas))
