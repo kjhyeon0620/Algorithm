5a87e0700c480ccfb5d528b3d16204b382e6cede
zzq3902 <48148683+zzq3902@users.noreply.github.com>
2024-07-18 14:22:58 +0900

    [Gold V] Title: 적록색약, Time: 160 ms, Memory: 111820 KB -BaekjoonHub

---
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -0,0 +1,41 @@
+# 색깔별로 탐색하여 구역의 수를 센다.
+# 적록색약의 경우 리스트를 복사하여 R을 전부 G로 바꾼 후 구역을 센다.
+
+import copy
+
+
+N = int(input())
+board_normal = [list(input()) for _ in range(N)]
+board_redGreen = copy.deepcopy(board_normal)
+
+for i in range(N):
+    for j in range(N):
+        if board_redGreen[i][j] == "R":
+            board_redGreen[i][j] = "G"
+
+
+def solution(area):
+    visited = [[False for _ in range(N)] for _ in range(N)]
+    dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
+    cnt = 0
+    for i in range(N):
+        for j in range(N):
+            if not visited[i][j]:
+                cnt += 1
+                visited[i][j] = True
+                stack = [[i, j]]
+                while stack:
+                    node = stack.pop()
+                    x, y = node[0], node[1],
+                    for k in range(4):
+                        nx = x + dx[k]
+                        ny = y + dy[k]
+                        if 0 <= nx < N and 0 <= ny < N and area[i][j] == area[nx][ny] and not visited[nx][ny]:
+                            stack.append([nx, ny])
+                            visited[nx][ny] = True
+    print(cnt, end=" ")
+
+
+
+solution(board_normal)
+solution(board_redGreen)
