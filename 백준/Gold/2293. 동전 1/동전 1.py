bb5c353e9cfa771c16ab767c9e1a957263d8d58d
zzq3902 <48148683+zzq3902@users.noreply.github.com>
2024-08-14 00:15:27 +0900

    [Gold V] Title: 동전 1, Time: 148 ms, Memory: 111512 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -1,18 +1,14 @@
-# 2차원 dp를 이용하여 해결한다.
-# dp[i][j]는 i번째 코인까지 사용하여 j의 가치를 만들 수 있는 경우의 수이다.
+# dp를 이용하여 해결한다
 
 n, k = map(int, input().split())
 coins = [int(input()) for _ in range(n)]
 
-dp = [[0 for _ in range(k+1)] for _ in range(2)]
-
-for i in range(1, n+1):
+dp = [0 for _ in range(k+1)]
+dp[0] = 1
+for i in range(n):
     for j in range(1, k+1):
-        if coins[i-1] == j:
-            dp[i % 2][j] = dp[(i+1) % 2][j] + 1
-        elif coins[i-1] <= j:
-            dp[(i % 2)][j] = dp[(i+1) % 2][j] + dp[i % 2][j-coins[i-1]]
-        else:
-            dp[i % 2][j] = dp[(i+1) % 2][j]
+        if coins[i] <= j:
+            dp[j] += dp[j-coins[i]]
 
-print(dp[n % 2][k])
+
+print(dp[-1])
