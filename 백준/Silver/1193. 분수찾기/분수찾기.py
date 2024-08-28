1613620649f45e3f5754f74d884f2f9a8869e6fc
zzq3902 <48148683+zzq3902@users.noreply.github.com>
2024-08-28 23:26:56 +0900

    [Silver V] Title: 분수찾기, Time: 84 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.py
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -1,25 +1,13 @@
-# 분자 : 1 1 2 3 2 1 1 2 3 4 5 4 3 2 1
-# 분모 : 1 2 1 1 2 3 4 3 2 1 1 2 3 4 5
-# 1 / 2,3 / 4,5,6 / 7,8,9,10/ 11,12,13,14,15
-# 1   2     3         4        5
-#           layer 짝수
-# 분자 : (X - start) + 1
-# 분모 : (end - X) + 1
-# layer가 홀수면 반대로
-
 X = int(input())
-end = 0
-layer = 1
+tmp = 1
 while True:
-    end += layer
-    if end >= X:
+    val = (tmp * (tmp + 1)) // 2
+    if val >= X:
         break
-    layer += 1
-start = end - layer + 1
-if layer % 2 == 0:
-    print(f'{X-start+1}/{end-X+1}')
+    tmp += 1
+num1 = X - val + tmp
+num2 = tmp - num1 + 1
+if tmp % 2 == 0:
+    print(f'{num1}/{num2}')
 else:
-    print(f'{end-X+1}/{X-start+1}')
-
-
-
+    print(f'{num2}/{num1}')
