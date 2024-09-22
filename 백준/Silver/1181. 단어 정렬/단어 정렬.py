e214c544d4ebd416d9da7ff8bcdb1a10cc5d4f3d
zzq3902 <48148683+zzq3902@users.noreply.github.com>
2024-09-22 22:24:04 +0900

    [Silver V] Title: 단어 정렬, Time: 164 ms, Memory: 114196 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,11 +1,4 @@
-# 데이터를 set에 저장하여 중복을 제거한다.
-# sort와 lambda를 활용하여 조건에 맞게 정렬한 후 출력한다.
-
-import sys
-
-
-input = sys.stdin.readline
-N = int(input())
-words = list(set(input().strip() for _ in range(N)))
-words.sort(key=lambda x: (len(x), x))
-print("\n".join(words))
+words = list(set(input() for _ in range(int(input()))))
+words.sort(key=lambda x: [len(x), x])
+for word in words:
+    print(word)
