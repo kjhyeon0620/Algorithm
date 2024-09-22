f48c09cd137cbb395ec2b6525afc70fd31a9c5c9
zzq3902 <48148683+zzq3902@users.noreply.github.com>
2024-09-22 20:54:49 +0900

    [Silver V] Title: 셀프 넘버, Time: 100 ms, Memory: 110480 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,25 +1,16 @@
-# d(n) 함수를 정의한다.
-# 10036 크기의 리스트를 만든 후 전부 True로 초기화한다. (d(9999) = 10035)
-# 1부터 10000까지 d(n)을 실행시켜 나온 수를 위 리스트에서 False로 바꾼다.
-
-import sys
-
-
-def dNum(num):
-    res = num
-
-    while num > 0:
-        res += num % 10
-        num //= 10
-
-    return res
+def d(n):
+    val = n
+    while n:
+        val += n % 10
+        n //= 10
+    return val
 
 
-isSelfNums = [True] * 10036
+nums = [True for _ in range(10036)]
 
 for i in range(1, 10001):
-    isSelfNums[dNum(i)] = False
+    nums[d(i)] = False
 
 for i in range(1, 10001):
-    if isSelfNums[i]:
-        sys.stdout.write(str(i) + "\n")
+    if nums[i]:
+        print(i)
