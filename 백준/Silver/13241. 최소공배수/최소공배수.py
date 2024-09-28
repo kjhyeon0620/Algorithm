89ebc16d54306478296f6b1c197094340877727e
zzq3902 <48148683+zzq3902@users.noreply.github.com>
2024-09-28 20:52:57 +0900

    [Silver V] Title: 최소공배수, Time: 84 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/13241. 최소공배수/최소공배수.py
+++ 백준/Silver/13241. 최소공배수/최소공배수.py
@@ -1,20 +1,7 @@
-# 최대공약수를 구하고 이를 이용하여 최소공배수를 구한다.
-
 A, B = map(int, input().split())
-
-_min, _max = min(A, B), max(A, B)
-cnt = 0
-divs = []
-
-for i in range(1, int(_min ** 0.5) + 1):
-    if _min % i == 0:
-        cnt += 1
-        divs.append(i)
-
-for i in range(cnt-1, -1, -1):
-    divs.append(_min // divs[i])
-
-for i in range(2*cnt-1, -1, -1):
-    if _max % divs[i] == 0:
-        print(divs[i] * (_max // divs[i]) * (_min // divs[i]))
-        break
+ans = A * B
+while A:
+    if A < B:
+        A, B = B, A
+    A %= B
+print(ans // B)
