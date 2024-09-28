8af854af3c8cc4224620742a8270c961fb4d7a1c
zzq3902 <48148683+zzq3902@users.noreply.github.com>
2024-09-28 20:37:01 +0900

    [Silver V] Title: 뒤집기, Time: 88 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,14 +1,9 @@
-# 0으로 이루어진 연속된 수의 갯수와 1로 이루어진 연속된 수의 갯수 중 작은 값이 정답이다.
-
 S = input()
-cnt = dict({"0": 0, "1": 0})
-prev = S[0]
+prev = "2"
+cnt = [0, 0]
+for num in S:
+    if num != prev:
+        cnt[int(num)] += 1
+        prev = num
 
-for ch in S:
-    if ch != prev:
-        cnt[prev] += 1
-        prev = ch
-        
-cnt[S[-1]] += 1 # 이전과 다른 경우에만 카운트되므로 마지막 덩어리는 처리되지 않기에 따로 처리한다. 
-# 
-print(cnt["0"] if cnt["0"] <= cnt["1"] else cnt["1"])
+print(cnt[0] if cnt[0] < cnt[1] else cnt[1])
