08031ff2c83ed907587bb58ed4952ecc9b6a0a70
zzq3902 <48148683+zzq3902@users.noreply.github.com>
2024-10-03 21:38:52 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 116 ms, Memory: 110820 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,21 +1,21 @@
-# 해당 칸의 인덱스를 i,j 라고 했을 때, i+j가 홀수 / 짝수인 경우로 나누어서 색이 맞는지 확인하면 된다.
+def findMin(startX, startY, b):
+    cnt = 0
+    for x in range(8):
+        for y in range(8):
+            if (x+y) % 2 == 0 and b[startY+y][startX+x] != "B":
+                cnt += 1
+            if (x+y) % 2 == 1 and b[startY+y][startX+x] != "W":
+                cnt += 1
+    return min(cnt, 64-cnt)
+
 
 N, M = map(int, input().split())
+board = [list(input()) for _ in range(N)]
 
-board = [input() for _ in range(N)]
-evenW = ["W", "B"]  # 왼쪽 위 첫 번째 칸이 "W"인 경우
-oddW = ["B", "W"]   # 왼쪽 위 첫 번째 칸이 "B"인 경우
+ans = []
 
-min_cnt = 32
-for i in range(N-7):
-    for j in range(M-7):
-        checkList = evenW if evenW[(i + j) % 2] == board[i][j] else oddW    # evenW와 oddW중 해당하는 체크리스트를 고른다
-        cnt = 0
-        for x in range(8):
-            for y in range(8):
-                if board[i+x][j+y] != checkList[(i+x+j+y) % 2]:
-                    cnt += 1
-        min_cnt = min(cnt, min_cnt, 64-cnt) # 바꿔야 하는 칸이 32를 넘어갈 경우는 위에서 결정한 체크리스트와 반대로 했을 때 
-                                            # 최소가 되는 것이기에 64에서 cnt를 빼준 것도 최솟값으로 고려해준다.
+for i in range(M-8+1):
+    for j in range(N-8+1):
+        ans.append(findMin(i, j, board))
 
-print(min_cnt)
+print(min(ans))
