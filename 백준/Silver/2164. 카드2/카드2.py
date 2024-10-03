83006fae5fc4948167008f1325bea68177159b14
zzq3902 <48148683+zzq3902@users.noreply.github.com>
2024-10-03 21:22:14 +0900

    [Silver IV] Title: 카드2, Time: 132 ms, Memory: 123048 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,13 +1,12 @@
-# deque의 popleft를 이용해서 문제를 해결한다.
-
 from collections import deque
 
+
 N = int(input())
-cards = deque(range(1, N+1))
+
+cards = deque([i for i in range(1, N+1)])
 
 for _ in range(N-1):
     cards.popleft()
     cards.append(cards.popleft())
 
-print(cards[0])
-
+print(cards.pop())
