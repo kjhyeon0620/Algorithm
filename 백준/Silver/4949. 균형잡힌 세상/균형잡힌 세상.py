471f10cde368ea1aed8c7eb4ae877d8d716d279f
zzq3902 <48148683+zzq3902@users.noreply.github.com>
2024-10-04 23:52:39 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 132 ms, Memory: 111976 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,33 +1,25 @@
-# stack을 이용하여 짝을 맞춘다.
-
 import sys
 
 
 input = sys.stdin.readline
 
 while True:
-    stack = []
-    inp = input().rstrip()
-    if inp == ".":
+    st = input().rstrip()
+    if st == ".":
         break
-    flag = True
-    for ch in inp:
-        if ch == "[" or ch == "(":
+    stack = []
+    ans = "yes"
+    for ch in st:
+        if ch == "(" or ch == "[":
             stack.append(ch)
-
-        elif ch == "]":
-            if len(stack) == 0 or stack.pop() != "[":
-                flag = False
-                break
-
         elif ch == ")":
-            if len(stack) == 0 or stack.pop() != "(":
-                flag = False
+            if not stack or stack.pop() != "(":
+                ans = "no"
+                break
+        elif ch == "]":
+            if not stack or stack.pop() != "[":
+                ans = "no"
                 break
     if stack:
-        flag = False
-
-    if flag:
-        print("yes")
-    else:
-        print("no")
+        ans = "no"
+    print(ans)
