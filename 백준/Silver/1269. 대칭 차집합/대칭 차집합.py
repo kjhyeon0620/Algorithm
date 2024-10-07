e32b6333a51cf82c53c023c337446b8c51362762
zzq3902 <48148683+zzq3902@users.noreply.github.com>
2024-10-07 16:07:17 +0900

    [Silver IV] Title: 대칭 차집합, Time: 208 ms, Memory: 175668 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -1,6 +1,11 @@
-# 대칭 차집합의 개수는 n(A) + n(B) - 2 * n(A, B의 교집합) 이다
-
 nA, nB = map(int, input().split())
 A = set(map(int, input().split()))
 B = set(map(int, input().split()))
-print(nA + nB - (2 * len(A.intersection(B))))
+
+cnt = 0
+
+for el in A:
+    if el in B:
+        cnt += 1
+
+print(nA + nB - 2 * cnt)
