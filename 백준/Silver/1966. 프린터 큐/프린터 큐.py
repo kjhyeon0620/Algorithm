8ba6d5ed80b4e800261e4ca91349b31c826b3d24
zzq3902 <48148683+zzq3902@users.noreply.github.com>
2024-10-15 15:57:22 +0900

    [Silver III] Title: 프린터 큐, Time: 144 ms, Memory: 114164 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -1,21 +1,20 @@
-# 중요도의 최댓값을 구한 후 큐에서 해당 최댓값의 중요도를 가진 문서가 나올때까지 문서를 뒤로 보낸다.
+import heapq
 from collections import deque
 
 for _ in range(int(input())):
     N, M = map(int, input().split())
-    documents = deque(map(int, input().split()))
-    idxList = deque(range(N))
-    cnt = 1
+    priority = list(map(int, input().split()))
+    queue = deque([i for i in range(N)])
+    ans = 1
+    maxHeap = [-p for p in priority]
+    heapq.heapify(maxHeap)
     while True:
-        _max = max(documents)
-        if documents[0] == _max:
-            if idxList[0] == M:
-                print(cnt)
+        if priority[queue[0]] == -maxHeap[0]:
+            if queue.popleft() == M:
+                print(ans)
                 break
             else:
-                documents.popleft()
-                idxList.popleft()
-                cnt += 1
+                heapq.heappop(maxHeap)
+                ans += 1
         else:
-            documents.append(documents.popleft())
-            idxList.append(idxList.popleft())
+            queue.append(queue.popleft())
