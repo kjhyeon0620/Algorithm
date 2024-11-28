d556389750d15256bfd0fdd20a011542c12282d6
Kim Juhyeon <48148683+zzq3902@users.noreply.github.com>
2024-11-28 23:47:58 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -5,16 +5,18 @@ isMinusExist = False
 for ch in exp:
     if ch.isalnum():
         tmp += ch
+    elif isMinusExist:
+        ans -= int(tmp)
+        tmp = ""
     else:
-        if isMinusExist:
-            ans -= int(tmp)
-        else:
-            if ch == "-":
-                isMinusExist = True
-            ans += int(tmp)
+        if ch == "-":
+            isMinusExist = True
+        ans += int(tmp)
         tmp = ""
 
 if isMinusExist:
     print(ans - int(tmp))
 else:
     print(ans + int(tmp))
+
+
