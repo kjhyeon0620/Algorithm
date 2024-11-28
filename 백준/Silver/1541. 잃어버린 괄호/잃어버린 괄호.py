5d3413617fdd7b054ebb54c8dabde903f4217683
Kim Juhyeon <48148683+zzq3902@users.noreply.github.com>
2024-11-28 23:46:39 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,25 +1,20 @@
-# -가 나오면 뒤에 있는 모든 수를 음수로 만들 수 있다.
-#(+가 나오면 괄호로 묶을 수 있음)
-
-nums = []
-ops = []
-num = ""
-flag = False
-for ch in input():
-    if ch == "+" or ch == "-":
-        nums.append(int(num))
-        num = ""
-        ops.append(ch)
+exp = input()
+tmp = ""
+ans = 0
+isMinusExist = False
+for ch in exp:
+    if ch.isalnum():
+        tmp += ch
     else:
-        num += ch
-nums.append(int(num))
-ans = nums[0]
+        if isMinusExist:
+            ans -= int(tmp)
+        else:
+            if ch == "-":
+                isMinusExist = True
+            ans += int(tmp)
+        tmp = ""
 
-for i in range(len(ops)):
-    if flag or ops[i] == "-":
-        flag = True
-        ans -= nums[i+1]
-    else:
-        ans += nums[i+1]
-
-print(ans)
+if isMinusExist:
+    print(ans - int(tmp))
+else:
+    print(ans + int(tmp))
