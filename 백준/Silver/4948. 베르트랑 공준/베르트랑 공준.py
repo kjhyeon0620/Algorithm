fcb422ced0013c5bcbda9eee964ee7298ff59504
Kim Juhyeon <48148683+zzq3902@users.noreply.github.com>
2025-01-09 20:18:07 +0900

    [Silver II] Title: 베르트랑 공준, Time: 248 ms, Memory: 112668 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,9 +1,4 @@
-# 1부터 i 까지의 소수의 개수를 미리 저장해둔다.
-# a 초과 b 이하의 소수의 개수 = f(b) - f(a)
-
 def isPrime(num):
-    if num == 1:
-        return False
     for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
             return False
@@ -14,7 +9,7 @@ memo = [-1 for _ in range(123456 * 2 + 1)]
 memo[0], memo[1] = 0, 0
 for i in range(2, 123456*2+1):
     if isPrime(i):
-        memo[i] = memo[i-1] +1
+        memo[i] = memo[i-1] + 1
     else:
         memo[i] = memo[i-1]
 
