7dafae27ceb2b4b7fbef9a2a4ddb59c9d3daf5b2
Kim Juhyeon <48148683+zzq3902@users.noreply.github.com>
2025-01-09 19:54:15 +0900

    [Silver II] Title: 랜선 자르기, Time: 116 ms, Memory: 110944 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,19 +1,17 @@
-# 이분탐색으로 해결한다.
-
-import sys
-
-
-input = sys.stdin.readline
 K, N = map(int, input().split())
-lengths = [int(input()) for _ in range(K)]
-left, right = 1, max(lengths)
+
+lans = [int(input()) for _ in range(K)]
+left, right = 1, max(lans)
+ans = 0
+
 while left <= right:
     mid = (left + right) // 2
-    total = 0
-    for length in lengths:
-        total += length // mid
-    if total >= N:
+    cnt = 0
+    for lan in lans:
+        cnt += lan // mid
+    if cnt >= N:
         left = mid + 1
-    elif total < N:
+    else:
         right = mid - 1
+
 print(right)
