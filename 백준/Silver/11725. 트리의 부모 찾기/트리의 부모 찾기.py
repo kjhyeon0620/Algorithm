d65a5665c27ae8877cca5bb76989976934443c0f
Kim Juhyeon <48148683+zzq3902@users.noreply.github.com>
2025-01-17 02:06:22 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 220 ms, Memory: 126256 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,32 +1,27 @@
-# bfs를 이용하여 그래프를 탐색하고, 깊이를 이용하여 각 노드의 부모를 저장한다.
-
-import sys
 from collections import deque
+import sys
 
 
 input = sys.stdin.readline
-N = int(input())
-graph = [[] for _ in range(N+1)]
 
+N = int(input())
+tree = [[] for _ in range(N+1)]
 for _ in range(N-1):
     n1, n2 = map(int, input().split())
-    graph[n1].append(n2)
-    graph[n2].append(n1)
-    
-parent = [-1 for _ in range(N+1)]
+    tree[n1].append(n2)
+    tree[n2].append(n1)
+
 visited = [False for _ in range(N+1)]
-depth = 0
-queue = deque()
-queue.append(1)
+ans = [-1 for _ in range(N+1)]
+queue = deque([1])
 
 while queue:
     node = queue.popleft()
     visited[node] = True
-    for el in graph[node]:
+    for el in tree[node]:
         if not visited[el]:
+            ans[el] = node
             queue.append(el)
-            parent[el] = node
 
 for i in range(2, N+1):
-    sys.stdout.write(str(parent[i]) + "\n")
-
+    print(ans[i])
