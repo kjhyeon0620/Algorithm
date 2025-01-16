836324dc73aa3deebfb0a19649c3c031e93de8bf
Kim Juhyeon <48148683+zzq3902@users.noreply.github.com>
2025-01-17 01:50:05 +0900

    [Silver II] Title: 좌표 압축, Time: 960 ms, Memory: 287408 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,20 +1,11 @@
-# set을 이용해 X에서 중복을 제거하고, 리스트에 저장한다.
-# 위 리스트를 정렬한다.
-# 위 리스트 값의 인덱스는 해당 값보다 적은 원소의 개수와 같다.
-import sys
-
-
-N = int(input())
-X = list(map(int, input().split()))
-
-
-newX = list(set(X))
-newX.sort()
+n = int(input())
+nums = list(map(int, input().split()))
+setList = list(set(nums))
+setList.sort()
 idx = dict()
 
-for i in range(len(newX)):
-    idx[newX[i]] = i
+for i in range(len(setList)):
+    idx[setList[i]] = i
 
-for x in X:
-    sys.stdout.write(str(idx[x]) +" ")
-
+for num in nums:
+    print(idx[num], end=" ")
