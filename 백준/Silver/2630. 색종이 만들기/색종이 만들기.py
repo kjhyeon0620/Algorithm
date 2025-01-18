ab1ac49772618aa1acd5bb39c91042af2b006e97
Kim Juhyeon <48148683+zzq3902@users.noreply.github.com>
2025-01-18 23:26:03 +0900

    [Silver II] Title: 색종이 만들기, Time: 108 ms, Memory: 111044 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,45 +1,24 @@
-import sys
-
-
-def check(x, y, k):
-    val = board[y][x]
-    global cntBlue
-    global cntWhite
-
-    for i in range(k):
-        for j in range(k):
-            if board[y+i][x+j] != val:  # 처음 색깔과 다른 색깔이 발견되면 즉시 False
-                return False
-
-    for i in range(k):                  # 모두 같은 색깔이므로 visited에 체크해줌
-        for j in range(k):
-            visited[y+i][x+j] = True
-    if val == 1:                        # 색깔에 맞게 count 증가
-        cntBlue += 1
-    else:
-        cntWhite += 1
-
-    return True
-
-
-input = sys.stdin.readline
 N = int(input())
-board = []
-visited = [[False for _ in range(N)] for _ in range(N)]
-for _ in range(N):
-    board.append(list(map(int, input().split())))
-tmp = N
-cntWhite = 0
-cntBlue = 0
-while True:
-    if tmp == 0:
-        break
-    for i in range(0, N, tmp):      # 쪼개는 크기에 맞게 모든 위치를 check 함수로 검사시킴
-        for j in range(0, N, tmp):
-            if not visited[j][i]:
-                check(i, j, tmp)
+board = [list(map(int, input().split())) for _ in range(N)]
+ans = [0, 0]
 
-    tmp //= 2
 
-print(cntWhite)
-print(cntBlue)
+def check(x, y, cnt):
+    color = board[x][y]
+
+    for i in range(cnt):
+        for j in range(cnt):
+            if board[x+i][y+j] != color:
+                cnt //= 2
+                for dx in range(2):
+                    for dy in range(2):
+                        check(x+(dx*cnt), y+(dy*cnt), cnt)
+                return
+
+    ans[color] += 1
+
+
+check(0, 0, N)
+
+for i in range(2):
+    print(ans[i])
