c3bf4e88ecfc28446c027b7fc4475c4686ebaf5b
Kim Juhyeon <48148683+zzq3902@users.noreply.github.com>
2025-01-23 22:57:26 +0900

    [Silver I] Title: RGB거리, Time: 104 ms, Memory: 110716 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,22 +1,9 @@
-# dp의 bottom-up을 이용하여 각 집의 색깔별로 최솟값을 저장해둔다.
-
-import sys
-
-
-input = sys.stdin.readline
 N = int(input())
-costs = []
-for _ in range(N):
-    costs.append(list(map(int, input().split())))
-dp = [[-1, -1, -1] for _ in range(N)]
-dp[0][0], dp[0][1], dp[0][2] = costs[0][0], costs[0][1], costs[0][2]
+cost = [list(map(int, input().split())) for _ in range(N)]
+dp = [[cost[0][0], cost[0][1], cost[0][2]] for _ in range(N)]
 
-for i in range(1, N):
-    for j in range(3):
-        tmp = []
-        for k in range(3):                # 해당 집을 특정 색깔로 칠했을 때,
-            if j != k:                    # 이전 집의 dp의 값중 같은 색깔을 제외한 후
-                tmp.append(dp[i-1][k])    # 작은 값을 골라 dp에 저장한다.
-        dp[i][j] = costs[i][j] + min(tmp)
+for home in range(1, N):
+    for color in range(3):
+        dp[home][color] = cost[home][color] + min(dp[home-1][(color+1) % 3], dp[home-1][(color+2) % 3])
 
 print(min(dp[N-1]))
