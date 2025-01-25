7dcf26ac074a7ac7df98dd83d854d14e7504fa57
Kim Juhyeon <48148683+zzq3902@users.noreply.github.com>
2025-01-25 20:15:11 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -1,15 +1,10 @@
-# dp[a][b] -> a번째 자리가 b인 경우 계단 수의 개수
-
 N = int(input())
-dp = [[1 for _ in range(10)] for _ in range(N+1)]
+dp = [[0 for _ in range(11)] for _ in range(N+1)]
 
+for a in range(1, 10):
+    dp[1][a] = 1
 for i in range(2, N+1):
     for j in range(10):
-        if j == 0:
-            dp[i][j] = dp[i-1][1]
-        elif j == 9:
-            dp[i][j] = dp[i-1][8]
-        else:
-            dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1]
+        dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
 
-print(sum(dp[N][1:]) % 1000000000)
+print(sum(dp[N]) % 1000000000)
