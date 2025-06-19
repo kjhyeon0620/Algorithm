d4c1c6a9b26277db9c6e5176a62d4ef6bfe5c33d
Kim Juhyeon <48148683+kjhyeon0620@users.noreply.github.com>
2025-06-19 16:54:42 +0900

    [Gold IV] Title: 최단경로, Time: 412 ms, Memory: 139056 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,5 +1,6 @@
-import math
 import heapq
+import math
+from collections import deque
 import sys
 
 input = sys.stdin.readline
@@ -7,7 +8,6 @@ V, E = map(int, input().split())
 K = int(input())
 graph = [[] for _ in range(V+1)]
 
-# 거리 배열 초기화
 distance = [math.inf] * (V+1)
 distance[K] = 0
 
@@ -15,18 +15,19 @@ for _ in range(E):
     u, v, w = map(int, input().split())
     graph[u].append((v, w))
 
-# 우선순위 큐(힙) 사용
-heap = [(0, K)]  # (거리, 노드)
+heap = [(0, K)]
 while heap:
     dist, node = heapq.heappop(heap)
     if dist > distance[node]:
-        continue  # 이미 더 짧은 거리로 방문한 경우 무시
+        continue
     for neighbor, weight in graph[node]:
         new_dist = dist + weight
         if new_dist < distance[neighbor]:
             distance[neighbor] = new_dist
             heapq.heappush(heap, (new_dist, neighbor))
 
-# 결과 출력
 for i in range(1, V+1):
-    print("INF" if math.isinf(distance[i]) else distance[i])
+    if math.isinf(distance[i]):
+        sys.stdout.write("INF\n")
+    else:
+        sys.stdout.write(str(distance[i]) + "\n")
